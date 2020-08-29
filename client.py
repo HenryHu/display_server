714b4871b59c79dde4f1943d67e30ec321be2fb3
Henry Hu <henry.hu.sh@gmail.com>
2020-08-29 16:22:09 -0400

    use urlencode

--- client.py
+++ client.py
@@ -2,31 +2,29 @@
 
 import sys
 import urllib.request
+import urllib.parse
 
 base_url = "http://dew/"
 
 def call(url, args={}):
     first = True
-    for key, value in args.items():
-        if first:
-            url += '?'
-            first = False
-        else:
-            url += '&'
-        url += '%s=%s' % (key, value)
+    if args: url += '?' + urllib.parse.urlencode(args)
     full_url = base_url + url
     print(full_url)
     print(urllib.request.urlopen(full_url).read())
 
 def notify(args):
     title = args[0]
-    call('notify/%s' % title)
+    call_args = {}
+    if len(args) > 1:
+        call_args['message'] = args[1]
+    call('notify/%s' % title, call_args)
 
 def additem(args):
     call('additem', {'title': args[0], 'content': args[1]})
 
 def delitem(args):
-    idx = args[0]
+    idx = int(args[0])
     call('delitem/%s' % idx)
 
 cmd = sys.argv[1]
