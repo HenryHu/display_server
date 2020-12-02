3e66e0e09dfbd4b119501ad67a8131f36868a42e
Henry Hu <henry.hu.sh@gmail.com>
2020-12-02 00:02:10 -0500

    support news and ping status

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import subprocess
 import glob
 import logging
+import collections
+
+import feedparser
 
 import dblib
 
@@ -11,9 +14,18 @@ from flask import Flask, request, render_template, g
 app = Flask("Display Server")
 db = dblib.DB(app)
 
+host_info = {}
+
 @app.teardown_appcontext
 def teardown(exception):
     db.teardown()
+
+def get_output(command, args):
+    app.logger.info("running %s %s" % (command, args))
+    try:
+        return subprocess.check_output(["env", "DISPLAY=:0", command, *args]).decode('utf-8')
+    except:
+        return None
 
 def run_command(command, args):
     app.logger.info("running %s %s" % (command, args))
@@ -58,14 +70,63 @@ def notify(title):
     else:
         return 'error'
 
+def collect_hosts():
+    targets = {}
+    maxlen = 0
+    for line in open('hosts.txt').read().split('\n'):
+        if not ' ' in line: continue
+        (name, target) = line.split(' ')
+        targets[name] = target
+        if len(name) > maxlen: maxlen = len(name)
+
+    hosts = []
+    for name, target in targets.items():
+        if not name in host_info:
+            host_info[name] = {"succ": 0, "total": 0, "history": []}
+
+        conn = "?"
+        info = host_info[name]
+        info["total"] += 1
+        if run_command("ping", ["-c", "1", "-W", "1", target]):
+            info["succ"] += 1
+            info["history"].append(1)
+
+            color = 'lightgreen'
+            if sum(info["history"]) != len(info['history']): color = "yellow"
+            conn = '<div style="display: inline; color: %s"> OK</div>' % color
+            style = "conn_good"
+        else:
+            conn = '<div style="display: inline; color: red">ERR</div>'
+            style = "conn_bad"
+            info["history"].append(0)
+
+        info["history"] = info["history"][-30:]
+        host = {
+            "name": ('%%-%ds' % maxlen) % name,
+            "conn": conn,
+            "style": style,
+            "rate": 1.0 * sum(info["history"]) / len(info["history"])
+        }
+        hosts.append(host)
+    return hosts
+
+def collect_news():
+    feed = feedparser.parse("https://hnrss.org/newest?count=8")
+    news = []
+    for item in feed['items']:
+        news.append({'title': item['title'], 'link': item['link'], 'content': ''})
+    return news
+
 @app.route('/page')
 def page():
     autorefresh = request.args.get('autorefresh', 'true')
 
+    hosts = collect_hosts()
+    news = collect_news()
     items = []
     for item in db.query_db("select * from items"):
         items.append(item)
-    return render_template('page.html', items=items, autorefresh=autorefresh)
+    return render_template('page.html', items=items, hosts=hosts, news=news, autorefresh=autorefresh)
 
 @app.route('/additem')
 def additem():
